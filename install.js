e92d4c41eb853bd9692f6b337caf6c01a597f557
Christian Broberg <cb@webhouse.dk>
2017-10-16 02:39:52 +0200

    Adding files

---
+++ install.js
@@ -0,0 +1,29 @@
+/*
+
+Bucket: react-notes-app-uploads
+
+Pool Id us-east-1_cayMvxHZ7
+Pool ARN arn:aws:cognito-idp:us-east-1:015874181742:userpool/us-east-1_cayMvxHZ7
+
+App client:
+notes-app
+
+App client id: 1nsrjatuh22cdcttskcov7dd0r
+
+npm install eslint aws-sdk serverless-offline --save-dev
+npm install --save-dev \
+    babel-core \
+    babel-loader \
+    babel-plugin-transform-runtime \
+    babel-preset-es2015 \
+    babel-preset-stage-3 \
+    serverless-webpack \
+    webpack \
+    webpack-node-externals
+
+npm install --save babel-runtime
+
+$ export SLS_DEBUG=true
+
+*/
+
